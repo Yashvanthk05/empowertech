b48d806e2ca8e73821892ad82d3beee744f99566
NAbinav <abinav0506@gmail.com>
2025-01-08 20:54:29 +0530

    Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
-import React, { useRef } from 'react'
-import Hero from '@/components/Hero'
-import Themes from '@/components/Themes'
-import Timeline from "@/components/TimelineComponent"
-import { useEffect } from 'react'
+import React, { useRef, useEffect } from 'react';
+import Hero from '@/components/Hero';
+import Themes from '@/components/Themes';
+import Timeline from "@/components/TimelineComponent";
+import Lenis from 'lenis';
 
-
-import Lenis from 'lenis'
-const page = () => {
+const Page = () => {
   const lenisRef = useRef(null);
+
   useEffect(() => {
     const lenis = new Lenis();
     lenisRef.current = lenis;
@@ -17,15 +16,16 @@ const page = () => {
       requestAnimationFrame(raf);
     }
     requestAnimationFrame(raf);
-  });
+  }, []);
 
   return (
     <>
-    <Hero/>
-    <Themes/>
-    <Timeline/>
+      <Hero />
+      <Themes />
+      <Timeline />
     </>
-  )
+  );
 }
 
-export default page
+export default Page;
+
